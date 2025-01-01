a676d01127c6f441c5ba11f7b1845b1a3d5e785d
Makara Chhin <cmpsnl310320@gmail.com>
2025-01-01 16:24:58 +0700

    Finished Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,9 +19,35 @@ const Navbar = () => {
         >
           {open ? "x" : "☰"}
         </div>
+        {/* MENU LINK LIST */}
+        <div
+          className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 bg-[#e6e6ff] transition-all ease-in-out ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="/">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login 👋
+            </button>
+          </a>
+        </div>
       </div>
       {/* DESKTOP MENU */}
-      <div className="hidden md:flex">D</div>
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="/">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+            Login 👋
+          </button>
+        </a>
+      </div>
     </div>
   );
 };
